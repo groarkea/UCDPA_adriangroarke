e65d03fdc93020b623cc0ea7af11a5ac85e96f62
groarkea <adrianwgroarke@hotmail.com>
2021-06-28 16:39:53 +0100

    Added test 2 for date range

--- test 2.py
+++ test 2.py
@@ -4,7 +4,8 @@ from datetime import datetime
 from datetime import date
 import matplotlib.pyplot as plt
 GS = pd.read_csv("GME_stock.csv")
-
+print(GS.columns
+      )
 missing_values_count = GS.isnull().sum()
 print(missing_values_count[0:10])
 
@@ -14,7 +15,7 @@ print(datetime(2020,9,20))
 print(GS['date'].head())
 print(type(GS['date']))
 
-ref_start = GS['date'] < '2021-01-01'
+ref_start = GS['date'] > datetime(2020-01-01)
 # ref_end = GS['date'] < datetime(2020,9,25)
 # dtrnge = ref_start & ref_end
 # GS.loc[dtrnge]
