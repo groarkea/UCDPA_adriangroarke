53560de016c8c96534ebbcdbeaa608be52e3a724
groarkea <adrianwgroarke@hotmail.com>
2021-07-02 17:17:51 +0100

    Added API for JPM Description, EPS and total assets for last 5 years

---
+++ Api_tab.py
@@ -0,0 +1,35 @@
+import requests
+data=requests.get('http://api.open-notify.org/iss-now.json')
+
+parsed_data=data.json() # looking for dictionary instead of string
+print(data.text)
+print(type(data.text))
+print(parsed_data)
+print(type(parsed_data)) # once dictionary then can use index []
+print(parsed_data['iss_position'])
+
+data2=requests.get('http://api.open-notify.org/astros.json')
+parsed_data2=data2.json()
+print(parsed_data2)
+for temp_variable in parsed_data2['people']:
+    print(temp_variable['name'])
+
+data4=requests.get('https://www.alphavantage.co/query?function=OVERVIEW&symbol=JPM&apikey=RVZ3KJWM4Q6RNVCJ')
+print(data4.text)
+print(type(data4))
+parsed_data4=data4.json()
+print(type(parsed_data4))
+output=parsed_data4['Description'] + ' The companies earnings per share is ' + parsed_data4['EPS']
+print(output)
+
+data5=requests.get('https://www.alphavantage.co/query?function=BALANCE_SHEET&symbol=JPM&apikey=RVZ3KJWM4Q6RNVCJ')
+print(data5.text)
+print(type(data5))
+parsed_data5 = data5.json()
+print(type(parsed_data5))
+
+parsed_data5a = parsed_data5['annualReports']
+print(parsed_data5a)
+
+for temp_variable in parsed_data5a:
+    print(temp_variable['fiscalDateEnding']) #'totalAssets'
--- Merge2TestGroupbyforIRR.py
+++ Merge2TestGroupbyforIRR.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import numpy_financial as npf
 
+
 import matplotlib.pyplot as plt
 
 # Import/Read 3 stock price datasets
