29140850181e4afdce9c76a41579228fe06eaebf
groarkea <adrianwgroarke@hotmail.com>
2021-07-13 11:08:53 +0100

    Created dataframe from Dictonary in API tab

--- Api_tab.py
+++ Api_tab.py
@@ -1,6 +1,7 @@
 import numpy as np
 import requests
-
+import pandas as pd
+#data for Gamestop to create dataframe from Dictionary
 data=requests.get('https://www.alphavantage.co/query?function=OVERVIEW&symbol=GME&apikey=RVZ3KJWM4Q6RNVCJ')
 print(data.text)
 print(type(data))
@@ -9,16 +10,32 @@ print(type(parsed_data))
 output="GameStop's 52 Week high is " + parsed_data['52WeekHigh'] + " and GameStop's 52 Week low is " + parsed_data['52WeekLow']
 print(output)
 
+#data for JPM to create dataframe from Dictionary
+dataJP=requests.get('https://www.alphavantage.co/query?function=OVERVIEW&symbol=JPM&apikey=RVZ3KJWM4Q6RNVCJ')
+print(dataJP.text)
+print(type(dataJP))
+parsed_dataJP=dataJP.json()
+print(type(parsed_dataJP))
+outputJP="JP Morgan's 52 Week high is " + parsed_dataJP['52WeekHigh'] + " and JP Morgan's 52 Week low is " + parsed_dataJP['52WeekLow']
+print(outputJP)
+
+#create dictionary
 a=parsed_data['Symbol']
 b=parsed_data['52WeekHigh']
 c=parsed_data['52WeekLow']
-my_dic={'ID':a,'52High':b,'52Low':c}
-print(my_dic)
-print(type(my_dic))
+a1=parsed_dataJP['Symbol']
+b1=parsed_dataJP['52WeekHigh']
+c1=parsed_dataJP['52WeekLow']
+my_dictnry={'ID':[a,a1],'52High':[b,b1],'52Low':[c,c1]}
+print(my_dictnry)
+print(type(my_dictnry))
 
+#create dataframe
+my_df= pd.DataFrame(my_dictnry)
+print(my_df)
+print(my_df['52High'])
 
-
-
+# API for JP Morgan description and EPS
 data4=requests.get('https://www.alphavantage.co/query?function=OVERVIEW&symbol=JPM&apikey=RVZ3KJWM4Q6RNVCJ')
 print(data4.text)
 print(type(data4))
@@ -27,6 +44,7 @@ print(type(parsed_data4))
 output=parsed_data4['Description'] + "' The company's earnings per share is '" + parsed_data4['EPS']
 print(output)
 
+# API for JP Morgan total assets reported annually
 data5=requests.get('https://www.alphavantage.co/query?function=BALANCE_SHEET&symbol=JPM&apikey=RVZ3KJWM4Q6RNVCJ')
 #print(data5.text)
 print(type(data5))
